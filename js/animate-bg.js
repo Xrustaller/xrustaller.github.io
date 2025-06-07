fd5bedf0c08d11325f37dce6af04f022a01366f6
Xrustaller <stalker0077@gmail.com>
2025-06-07 19:30:50 +0300

    Update animate-bg.js

--- js/animate-bg.js
+++ js/animate-bg.js
@@ -1,123 +1,142 @@
 const refreshDuration = 5000;
-let refreshTimeout;
-let numPointsX;
-let numPointsY;
-let unitWidth;
-let unitHeight;
-let points;
+let lastRefresh = 0;
+let numPointsX, numPointsY, unitWidth, unitHeight, points = [], polygons = [];
 
 $(function () {
-    const $svgContainer = $('<div id="bg" style="pointer-events: none;"></div>');
+    const $svgContainer = $('<div id="bg"></div>');
     $('header').after($svgContainer);
+
+    $(window).on('resize', onResize);
+    onLoad();
+    requestAnimationFrame(tick);
 });
 
-$(window).on('load', onLoad);
-$(window).on('resize', onResize);
+function getViewportSize() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+}
 
 function onLoad() {
-    const $svg = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
-    $svg.attr('width', $(window).width());
-    $svg.attr('height', $(window).height() - 4);
-    $('#bg').append($svg);
+    const viewport = getViewportSize();
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', viewport.width);
+    svg.setAttribute('height', viewport.height - 4);
+    $('#bg').empty().append(svg);
 
-    const unitSize = ($(window).width() + $(window).height()) / 40;
-    numPointsX = Math.ceil($(window).width() / unitSize) + 1;
-    numPointsY = Math.ceil($(window).height() / unitSize) + 1;
-    unitWidth = Math.ceil($(window).width() / (numPointsX - 1));
-    unitHeight = Math.ceil($(window).height() / (numPointsY - 1));
-
+    const unitSize = (viewport.width + viewport.height) / 40;
+    numPointsX = Math.ceil(viewport.width / unitSize) + 1;
+    numPointsY = Math.ceil(viewport.height / unitSize) + 1;
+    unitWidth = Math.ceil(viewport.width / (numPointsX - 1));
+    unitHeight = Math.ceil(viewport.height / (numPointsY - 1));
     points = [];
 
     for (let y = 0; y < numPointsY; y++) {
         for (let x = 0; x < numPointsX; x++) {
-            points.push({x: unitWidth * x, y: unitHeight * y, originX: unitWidth * x, originY: unitHeight * y});
+            const px = unitWidth * x;
+            const py = unitHeight * y;
+
+            // Случайное смещение внутри одной ячейки
+            const offsetX = (x > 0 && x < numPointsX - 1) ? (Math.random() * unitWidth - unitWidth / 2) : 0;
+            const offsetY = (y > 0 && y < numPointsY - 1) ? (Math.random() * unitHeight - unitHeight / 2) : 0;
+
+            const startX = px + offsetX;
+            const startY = py + offsetY;
+
+            points.push({
+                originX: px,
+                originY: py,
+                x: startX,
+                y: startY,
+                prevX: startX,
+                prevY: startY,
+                targetX: startX,
+                targetY: startY,
+            });
         }
     }
 
-    randomize();
+    const ns = svg.namespaceURI;
+    polygons = [];
 
     for (let i = 0; i < points.length; i++) {
         if (points[i].originX !== unitWidth * (numPointsX - 1) && points[i].originY !== unitHeight * (numPointsY - 1)) {
-            const topLeftX = points[i].x;
-            const topLeftY = points[i].y;
-            const topRightX = points[i + 1].x;
-            const topRightY = points[i + 1].y;
-            const bottomLeftX = points[i + numPointsX].x;
-            const bottomLeftY = points[i + numPointsX].y;
-            const bottomRightX = points[i + numPointsX + 1].x;
-            const bottomRightY = points[i + numPointsX + 1].y;
-
             const rando = Math.floor(Math.random() * 1.5);
-
             for (let n = 0; n < 2; n++) {
-                const polygon = document.createElementNS($svg[0].namespaceURI, 'polygon');
+                const polygon = document.createElementNS(ns, 'polygon');
+                let p1, p2, p3;
 
                 if (rando === 0) {
-                    if (n === 0) {
-                        polygon.point1 = i;
-                        polygon.point2 = i + numPointsX;
-                        polygon.point3 = i + numPointsX + 1;
-                        polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + bottomRightX + ',' + bottomRightY);
-                    } else if (n === 1) {
-                        polygon.point1 = i;
-                        polygon.point2 = i + 1;
-                        polygon.point3 = i + numPointsX + 1;
-                        polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + topRightX + ',' + topRightY + ' ' + bottomRightX + ',' + bottomRightY);
-                    }
-                } else if (rando === 1) {
-                    if (n === 0) {
-                        polygon.point1 = i;
-                        polygon.point2 = i + numPointsX;
-                        polygon.point3 = i + 1;
-                        polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + topRightX + ',' + topRightY);
-                    } else if (n === 1) {
-                        polygon.point1 = i + numPointsX;
-                        polygon.point2 = i + 1;
-                        polygon.point3 = i + numPointsX + 1;
-                        polygon.setAttribute('points', bottomLeftX + ',' + bottomLeftY + ' ' + topRightX + ',' + topRightY + ' ' + bottomRightX + ',' + bottomRightY);
-                    }
+                    if (n === 0) [p1, p2, p3] = [i, i + numPointsX, i + numPointsX + 1];
+                    else [p1, p2, p3] = [i, i + 1, i + numPointsX + 1];
+                } else {
+                    if (n === 0) [p1, p2, p3] = [i, i + numPointsX, i + 1];
+                    else [p1, p2, p3] = [i + numPointsX, i + 1, i + numPointsX + 1];
                 }
-                polygon.setAttribute('fill', 'rgba(0,0,0,' + (Math.random() / 2) + ')');
-                const animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
-                animate.setAttribute('fill', 'freeze');
-                animate.setAttribute('attributeName', 'points');
-                animate.setAttribute('dur', refreshDuration + 'ms');
-                animate.setAttribute('calcMode', 'linear');
-                polygon.appendChild(animate);
-                $svg.append(polygon);
+
+                polygon.point1 = p1;
+                polygon.point2 = p2;
+                polygon.point3 = p3;
+                polygon.setAttribute('fill', `rgba(0,0,0,${Math.random() / 2})`);
+                svg.appendChild(polygon);
+                polygons.push(polygon);
             }
         }
     }
 
-    refresh();
+    randomizeTargets();
 }
 
-function randomize() {
-    for (let i = 0; i < points.length; i++) {
-        if (points[i].originX !== 0 && points[i].originX !== unitWidth * (numPointsX - 1)) {
-            points[i].x = points[i].originX + Math.random() * unitWidth - unitWidth / 2;
+function randomizeTargets() {
+    for (const p of points) {
+        p.prevX = p.x;
+        p.prevY = p.y;
+
+        if (p.originX !== 0 && p.originX !== unitWidth * (numPointsX - 1)) {
+            p.targetX = p.originX + Math.random() * unitWidth - unitWidth / 2;
+        } else {
+            p.targetX = p.originX;
         }
-        if (points[i].originY !== 0 && points[i].originY !== unitHeight * (numPointsY - 1)) {
-            points[i].y = points[i].originY + Math.random() * unitHeight - unitHeight / 2;
+
+        if (p.originY !== 0 && p.originY !== unitHeight * (numPointsY - 1)) {
+            p.targetY = p.originY + Math.random() * unitHeight - unitHeight / 2;
+        } else {
+            p.targetY = p.originY;
         }
     }
 }
 
-function refresh() {
-    randomize();
-    $('#bg svg').children().each(function(index, polygon) {
-        const animate = $(polygon).children().first()[0];
-        if (animate.getAttribute('to')) {
-            animate.setAttribute('from', animate.getAttribute('to'));
-        }
-        animate.setAttribute('to', points[polygon.point1].x + ',' + points[polygon.point1].y + ' ' + points[polygon.point2].x + ',' + points[polygon.point2].y + ' ' + points[polygon.point3].x + ',' + points[polygon.point3].y);
-        animate.beginElement();
-    });
-    refreshTimeout = setTimeout(refresh, refreshDuration);
+function updatePolygons(t) {
+    const progress = Math.min((t - lastRefresh) / refreshDuration, 1);
+
+    for (const p of points) {
+        p.x = p.prevX + (p.targetX - p.prevX) * progress;
+        p.y = p.prevY + (p.targetY - p.prevY) * progress;
+    }
+
+    for (const poly of polygons) {
+        poly.setAttribute(
+            'points',
+            `${points[poly.point1].x},${points[poly.point1].y} ${points[poly.point2].x},${points[poly.point2].y} ${points[poly.point3].x},${points[poly.point3].y}`
+        );
+    }
+}
+
+function tick(t) {
+    if (!lastRefresh) lastRefresh = t;
+
+    updatePolygons(t);
+
+    if (t - lastRefresh > refreshDuration) {
+        lastRefresh = t;
+        randomizeTargets();
+    }
+
+    requestAnimationFrame(tick);
 }
 
 function onResize() {
-    $('#bg svg').remove();
-    clearTimeout(refreshTimeout);
     onLoad();
+    lastRefresh = performance.now();
 }
